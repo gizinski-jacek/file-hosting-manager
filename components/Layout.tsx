775cb4b71cb34f3bf05d8a88b4eb1adb45319931
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-10 17:01:05 +0200

    Wrapping entier app with UserProvider

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,19 +2,22 @@ import styles from '../styles/Layout.module.scss';
 import Head from 'next/head';
 import Nav from './Nav';
 import Footer from './Footer';
+import { UserProvider } from '../hooks/UserProvider';
 
 const Layout = ({ children }: React.PropsWithChildren) => {
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>File Hosting Manager</title>
-				<meta name='description' content='File Hosting Manager' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<Nav />
-			<main className={styles.main}>{children}</main>
-			<Footer />
-		</div>
+		<UserProvider>
+			<div className={styles.container}>
+				<Head>
+					<title>File Hosting Manager</title>
+					<meta name='description' content='File Hosting Manager' />
+					<link rel='icon' href='/favicon.ico' />
+				</Head>
+				<Nav />
+				<main className={styles.main}>{children}</main>
+				<Footer />
+			</div>
+		</UserProvider>
 	);
 };
 
