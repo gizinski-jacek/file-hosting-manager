ac2d9f5910310498df73058fb9b56c1636f119f3
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-12 16:46:17 +0200

    Updating user schema.
    Adjusting values min/max lengths.
    Adding api_data array for storing api keys for each host.

--- models/user.ts
+++ models/user.ts
@@ -7,18 +7,35 @@ const UserSchema = new Schema(
 	{
 		email: {
 			type: Schema.Types.Email,
-			minlength: 8,
-			maxlength: 64,
+			minlength: 4,
+			maxlength: 32,
 			required: true,
 			select: false,
 		},
 		username: { type: String, minlength: 2, maxlength: 32, required: true },
 		password: {
 			type: String,
-			minlength: 8,
-			maxlength: 64,
+			minlength: 4,
+			maxlength: 128,
 			required: true,
 			select: false,
+		},
+		api_data: {
+			type: [
+				{
+					host: {
+						type: String,
+						minlength: 4,
+						maxlength: 32,
+					},
+					email: {
+						type: Schema.Types.Email,
+						minlength: 4,
+						maxlength: 32,
+					},
+					api_key: { type: String, minlength: 8, maxlength: 128 },
+				},
+			],
 		},
 	},
 	{ timestamps: true }
