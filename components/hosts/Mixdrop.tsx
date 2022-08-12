a92d0dfa4fd11c4345a1580be5d22d0d8b372698
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-12 16:54:38 +0200

    Adding components for each file host.
    Checking for passed api_data prop to render form or data from api.
    Handling input change and for submission

---
+++ components/hosts/Mixdrop.tsx
@@ -0,0 +1,87 @@
+import {
+	Button,
+	FormControl,
+	FormGroup,
+	Input,
+	InputLabel,
+} from '@mui/material';
+import axios from 'axios';
+import { useState } from 'react';
+
+interface FormData {
+	api_key: string;
+	[key: string]: string;
+}
+
+interface Props extends FormData {
+	host: string;
+}
+
+const Mixdrop = ({ api_data }: Props | undefined) => {
+	const [formData, setFormData] = useState<FormData>({
+		api_key: '',
+		email: '',
+	});
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
+
+	const handleFormSubmit = async () => {
+		try {
+			const res = await axios.post(
+				'/api/user/key-update?host=mixdrop',
+				formData,
+				{
+					withCredentials: true,
+				}
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	console.log(formData);
+	return (
+		<div>
+			{api_data ? (
+				'render data'
+			) : (
+				<FormGroup>
+					<FormControl>
+						<InputLabel htmlFor='email'>Input your API email</InputLabel>
+						<Input
+							id='email'
+							name='email'
+							inputProps={{ minLength: 4, maxLength: 64 }}
+							value={formData.email}
+							onChange={handleInputChange}
+							required
+							placeholder='API email'
+						/>
+					</FormControl>
+					<FormControl>
+						<InputLabel htmlFor='api_key'>Input your API key</InputLabel>
+						<Input
+							id='api_key'
+							name='api_key'
+							inputProps={{ minLength: 4, maxLength: 64 }}
+							value={formData.api_key}
+							onChange={handleInputChange}
+							required
+							placeholder='API key'
+						/>
+					</FormControl>
+					<Button type='button' onClick={handleFormSubmit}>
+						Submit
+					</Button>
+				</FormGroup>
+			)}
+		</div>
+	);
+};
+
+export default Mixdrop;
