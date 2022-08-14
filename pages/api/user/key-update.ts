ef3fc3e2f16a51c36121168236202e06140d6519
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-15 00:20:39 +0200

    Fixing, updating and cleaning up.
    Wrapping everything in trycatch block.
    Moving types to separate files and importing them.
    Protecting with getServerSession instead of getToken.
    Fixing incorrect payload.

--- pages/api/user/key-update.ts
+++ pages/api/user/key-update.ts
@@ -1,106 +1,107 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import * as Yup from 'yup';
-import yupValidation from '../../../lib/yupValidation';
 import connectMongo from '../../../lib/mongodb';
 import User from '../../../models/user';
 import jwt from 'jsonwebtoken';
-import { getToken } from 'next-auth/jwt';
-
-interface UserModel {
-	email: string;
-	username: string;
-	password: string;
-}
-
-interface UserSignUpModel extends UserModel {
-	repeat_password: string;
-}
+import { unstable_getServerSession } from 'next-auth';
+import { nextAuthOptions } from '../auth/[...nextauth]';
+import { MongoUserModel, TempUserToken } from '../../../lib/types/types';
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<{ [key: string]: string | boolean } | string>
+	res: NextApiResponse
 ) {
-	if (req.method === 'POST') {
-		const { host } = req.query as { host: string };
-		if (!host) {
-			return res.status(404).json('Error, host not found');
-		}
-		if (!process.env.JWT_STRATEGY_SECRET || !process.env.NEXTAUTH_SECRET) {
-			return res.status(404).json('Server error');
-		}
-		if (req.cookies['next-auth.session-token']) {
-			const decodedToken = await getToken({ req });
-			await connectMongo();
-			const userExists = await User.findById(decodedToken.user._id).exec();
-			if (!userExists) {
-				return res.status(404).json('User not found');
+	try {
+		if (req.method === 'POST') {
+			const { host } = req.query as { host: string };
+			if (!host) {
+				return res.status(404).json('Error, host not found');
 			}
-			if (userExists.api_data.find((d) => d.host === host)) {
-				const newData = {
-					...req.body,
-					host: host,
-				};
-				const updatedUser = await User.findByIdAndUpdate(
-					userExists._id,
-					{ $set: { 'api_data.$[el]': newData } },
-					{
-						arrayFilters: [{ 'el.host': host }],
-						timestamps: true,
-						new: true,
-					}
+			const session = await unstable_getServerSession(
+				req,
+				res,
+				nextAuthOptions
+			);
+			if (session) {
+				await connectMongo();
+				const user: MongoUserModel = await User.findById(
+					session.user?._id
 				).exec();
-				if (!updatedUser) {
-					return res.status(404).json('Failed to update user');
+				if (!user) {
+					return res.status(404).json('User not found');
 				}
+				const apiData = user.api_data.find((d) => d.host === host);
+				if (apiData) {
+					const newData = {
+						...req.body,
+						host: host,
+					};
+					const updatedUser = await User.findByIdAndUpdate(
+						user._id,
+						{ $set: { 'api_data.$[el]': newData } },
+						{
+							arrayFilters: [{ 'el.host': host }],
+							timestamps: true,
+							new: true,
+						}
+					).exec();
+					if (!updatedUser) {
+						return res.status(404).json('Failed to update user');
+					}
+					return res.status(200).json({ success: true });
+				} else {
+					const newData = {
+						...req.body,
+						host: host,
+					};
+					const updatedUser = await User.findByIdAndUpdate(
+						user._id,
+						{ $addToSet: { api_data: newData } },
+						{
+							upsert: true,
+							timestamps: true,
+							new: true,
+						}
+					).exec();
+					if (!updatedUser) {
+						return res.status(404).json('Failed to update user');
+					}
+					return res.status(200).json({ success: true });
+				}
+			} else if (req.cookies.tempUserToken) {
+				if (!process.env.JWT_STRATEGY_SECRET) {
+					return res.status(404).json('Server error');
+				}
+				const decodedToken: TempUserToken = jwt.verify(
+					req.cookies.tempUserToken,
+					process.env.JWT_STRATEGY_SECRET
+				);
+				const payload = {
+					api_data: [...decodedToken.api_data, { host: host, ...req.body }],
+				};
+				const token = jwt.sign(payload, process.env.JWT_STRATEGY_SECRET);
+				res.setHeader(
+					'Set-Cookie',
+					`tempUserToken=${token}; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=3600` // 60 minutes
+				);
+				return res.status(200).json({ success: true });
 			} else {
-				const newData = {
-					...req.body,
-					host: host,
-				};
-				const updatedUser = await User.findByIdAndUpdate(
-					userExists._id,
-					{ $addToSet: { api_data: newData } },
-					{
-						upsert: true,
-						timestamps: true,
-						new: true,
-					}
-				).exec();
-				if (!updatedUser) {
-					return res.status(404).json('Failed to update user');
+				if (!process.env.JWT_STRATEGY_SECRET) {
+					return res.status(404).json('Server error');
 				}
+				const payload = {
+					api_data: [{ host: host, ...req.body }],
+				};
+				const token = jwt.sign(payload, process.env.JWT_STRATEGY_SECRET);
+				res.setHeader(
+					'Set-Cookie',
+					`tempUserToken=${token}; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=3600` // 60 minutes
+				);
+				return res.status(200).json({ success: true });
 			}
-			return res.status(200).json({ success: true });
-		} else if (req.cookies.tempUserToken) {
-			const decodedToken = jwt.verify(
-				req.cookies.tempUserToken,
-				process.env.JWT_STRATEGY_SECRET
-			);
-			const payload = {
-				...decodedToken,
-				[host]: req.body,
-			};
-			const token = jwt.sign(payload, process.env.JWT_STRATEGY_SECRET);
-			res.setHeader(
-				'Set-Cookie',
-				`tempUserToken=${token}; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=3600` // 60 minutes
-			);
-			return res.status(200).json({ success: true });
-		} else {
-			const payload = {
-				[host]: req.body,
-			};
-			if (!process.env.JWT_STRATEGY_SECRET) {
-				return res.status(404).json('Server error');
-			}
-			const token = jwt.sign(payload, process.env.JWT_STRATEGY_SECRET);
-			res.setHeader(
-				'Set-Cookie',
-				`tempUserToken=${token}; Path=/; httpOnly=true; SameSite=strict; Secure=true; Max-Age=3600` // 60 minutes
-			);
-			return res.status(200).json({ success: true });
 		}
+		return res.status(404).json('No endpoint');
+	} catch (error) {
+		return res.status(404).json(error);
 	}
-	return res.status(200).json({ success: false });
 }
