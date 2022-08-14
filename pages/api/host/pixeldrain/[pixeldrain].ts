abe9f65404921fe9033b833dbd61b94d7d072dd7
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-15 00:17:50 +0200

    Handling not signed in users.
    Importing types.
    Handling not signed in users with temporary tokens.

--- pages/api/host/pixeldrain/[pixeldrain].ts
+++ pages/api/host/pixeldrain/[pixeldrain].ts
@@ -3,8 +3,10 @@ import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import connectMongo from '../../../../lib/mongodb';
+import { MongoUserModel, TempUserToken } from '../../../../lib/types/types';
 import User from '../../../../models/user';
 import { nextAuthOptions } from '../../auth/[...nextauth]';
+import jwt from 'jsonwebtoken';
 
 export default async function handler(
 	req: NextApiRequest,
@@ -16,7 +18,7 @@ export default async function handler(
 			if (req.method === 'GET') {
 				if (req.query.pixeldrain === 'get-user-files') {
 					await connectMongo();
-					const user = await User.findById(session.user?._id)
+					const user: MongoUserModel = await User.findById(session.user?._id)
 						.select('+api_data')
 						.exec();
 					if (!user) {
@@ -43,7 +45,7 @@ export default async function handler(
 				}
 				if (req.query.pixeldrain === 'get-user-folders') {
 					await connectMongo();
-					const user = await User.findById(session.user?._id)
+					const user: MongoUserModel = await User.findById(session.user?._id)
 						.select('+api_data')
 						.exec();
 					if (!user) {
@@ -118,9 +120,79 @@ export default async function handler(
 				// 	);
 				// }
 			}
-			return res.status(404).json({ success: false });
+			return res.status(404).json('No endpoint');
+		} else if (req.cookies.tempUserToken) {
+			if (req.method === 'GET') {
+				if (req.query.pixeldrain === 'get-user-files') {
+					if (!process.env.JWT_STRATEGY_SECRET) {
+						return res.status(404).json('Server error');
+					}
+					const decodedToken: TempUserToken = jwt.verify(
+						req.cookies.tempUserToken,
+						process.env.JWT_STRATEGY_SECRET
+					);
+					const userAPIData = decodedToken.api_data.find(
+						(d) => d.host === 'pixeldrain'
+					);
+					if (!userAPIData) {
+						return res.status(404).json('No api data');
+					}
+					if (!userAPIData.api_key) {
+						return res.status(404).json('No api key');
+					}
+					const apiRes = await axios.get(
+						'https://pixeldrain.com/api/user/files',
+						{
+							headers: {
+								Authorization: `Basic ${btoa(':' + userAPIData.api_key)}`,
+							},
+						}
+					);
+					return res.status(200).json(apiRes.data.files);
+				}
+				if (req.query.pixeldrain === 'get-user-folders') {
+					if (!process.env.JWT_STRATEGY_SECRET) {
+						return res.status(404).json('Server error');
+					}
+					const decodedToken: TempUserToken = jwt.verify(
+						req.cookies.tempUserToken,
+						process.env.JWT_STRATEGY_SECRET
+					);
+					const userAPIData = decodedToken.api_data.find(
+						(d) => d.host === 'pixeldrain'
+					);
+					if (!userAPIData) {
+						return res.status(404).json('No api data');
+					}
+					if (!userAPIData.api_key) {
+						return res.status(404).json('No api key');
+					}
+					const apiRes = await axios.get(
+						'https://pixeldrain.com/api/user/lists',
+						{
+							headers: {
+								Authorization: `Basic ${btoa(':' + userAPIData.api_key)}`,
+							},
+						}
+					);
+					return res.status(200).json(apiRes.data.lists);
+				}
+				if (req.query.pixeldrain === 'get-single-file') {
+					const apiRes = await axios.get(
+						`https://pixeldrain.com/api/file/${req.query.id}?download`,
+						{ responseType: 'blob' }
+					);
+					return res.status(200).json(apiRes.data);
+				}
+				if (req.query.pixeldrain === 'get-single-folder') {
+					const apiRes = await axios.get(
+						`https://pixeldrain.com/api/list/${req.query.id}`
+					);
+					return res.status(200).json(apiRes.data.files);
+				}
+			}
 		} else {
-			return res.status(401).json('Unauthorized access');
+			return res.status(404).json({ success: false });
 		}
 	} catch (error) {
 		return res.status(404).json(error);
