797a99e3f4e98c590e978cfe039b7cbb784fc095
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-15 00:18:37 +0200

    Handling not signed in users.
    Importing types.
    Handling not signed in users with temporary tokens.

--- pages/api/user/has-key.ts
+++ pages/api/user/has-key.ts
@@ -2,8 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import connectMongo from '../../../lib/mongodb';
+import { MongoUserModel, TempUserToken } from '../../../lib/types/types';
 import User from '../../../models/user';
 import { nextAuthOptions } from '../auth/[...nextauth]';
+import jwt from 'jsonwebtoken';
 
 export default async function handler(
 	req: NextApiRequest,
@@ -11,23 +13,40 @@ export default async function handler(
 ) {
 	try {
 		const session = await unstable_getServerSession(req, res, nextAuthOptions);
+		const { host } = req.query as { host: string };
 		if (session) {
 			if (req.method === 'GET') {
-				const { host } = req.query as { host: string };
 				await connectMongo();
-				const user = await User.findById(session.user?._id)
+				const user: MongoUserModel = await User.findById(session.user?._id)
 					.select('+api_data')
 					.exec();
-				const apiData = user.api_data.find((d) => d.host === host);
-				if (!apiData || !apiData.api_key) {
+				if (!user) {
+					return res.status(404).json('User not found');
+				}
+				const userAPIData = user.api_data.find((d) => d.host === host);
+				if (!userAPIData || !userAPIData.api_key) {
 					return res.status(200).json(false);
 				} else {
 					return res.status(200).json(true);
 				}
 			}
-			return res.status(404).json({ success: false });
+			return res.status(404).json('No endpoint');
+		} else if (req.cookies.tempUserToken) {
+			if (!process.env.JWT_STRATEGY_SECRET) {
+				return res.status(404).json('Server error');
+			}
+			const decodedToken: TempUserToken = jwt.verify(
+				req.cookies.tempUserToken,
+				process.env.JWT_STRATEGY_SECRET
+			);
+			const userAPIData = decodedToken.api_data.find((d) => d.host === host);
+			if (!userAPIData || !userAPIData.api_key) {
+				return res.status(200).json(false);
+			} else {
+				return res.status(200).json(true);
+			}
 		} else {
-			return res.status(401).json('Unauthorized access');
+			return res.status(200).json(false);
 		}
 	} catch (error) {
 		return res.status(404).json(error);
