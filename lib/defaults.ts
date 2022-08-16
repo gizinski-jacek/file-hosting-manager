c86c3debee6975e1217c87e6196a6e2713d05015
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-16 18:06:06 +0200

    Adding file with default values used for forms.

---
+++ lib/defaults.ts
@@ -0,0 +1,31 @@
+export const defaultSignInValues = {
+	username_or_email: '',
+	password: '',
+};
+
+export const defaultSignUpValues = {
+	email: '',
+	username: '',
+	password: '',
+	confirm_password: '',
+};
+
+export const defaultKeysDataValues = [
+	{
+		host: 'gofile',
+		api_key: '',
+	},
+	{
+		host: 'pixeldrain',
+		api_key: '',
+	},
+	{
+		host: 'mixdrop',
+		api_key: '',
+		email: '',
+	},
+	{
+		host: 'anonfiles',
+		api_key: '',
+	},
+];
