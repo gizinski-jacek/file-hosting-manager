228a061fe6795b19c46f50ee1eed1cb030c3c42b
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-17 18:38:14 +0200

    Adding provider managing selected host to render

---
+++ hooks/HostProvider.tsx
@@ -0,0 +1,35 @@
+import { createContext, useCallback, useState } from 'react';
+
+interface Props {
+	children: React.ReactNode;
+}
+
+type HostProps = {
+	host: string | null;
+	updateHost: (value: string | null) => void;
+};
+
+const HostContext = createContext<HostProps>({
+	host: null,
+	updateHost: (value) => null,
+});
+
+const HostContextProvider = ({ children }: Props) => {
+	const [host, setHost] = useState<string | null>(null);
+
+	const updateHost = (value: string | null) => {
+		if (value) {
+			setHost(value.toLowerCase());
+		} else {
+			setHost(value);
+		}
+	};
+
+	return (
+		<HostContext.Provider value={{ host, updateHost }}>
+			{children}
+		</HostContext.Provider>
+	);
+};
+
+export { HostContext, HostContextProvider };
