f2be55040893820dfc8436eeaec81de88fa19a7a
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-17 18:34:38 +0200

    Implementing Host and Theme Context providers

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,6 +4,8 @@ import Head from 'next/head';
 import Nav from './Nav';
 import Footer from './Footer';
 import styles from '../styles/Layout.module.scss';
+import { HostContextProvider } from '../hooks/HostProvider';
+import { ThemeContextProvider } from '../hooks/ThemeProvider';
 
 interface Props {
 	children: React.ReactNode;
@@ -13,16 +15,20 @@ interface Props {
 const Layout = ({ children, session }: Props) => {
 	return (
 		<SessionProvider session={session}>
-			<div className={styles.container}>
-				<Head>
-					<title>File Hosting Manager</title>
-					<meta name='description' content='File Hosting Manager' />
-					<link rel='icon' href='/favicon.ico' />
-				</Head>
-				<Nav />
-				<main className={styles.main}>{children}</main>
-				<Footer />
-			</div>
+			<HostContextProvider>
+				<ThemeContextProvider>
+					<div className={styles.container}>
+						<Head>
+							<title>File Hosting Manager</title>
+							<meta name='description' content='File Hosting Manager' />
+							<link rel='icon' href='/favicon.ico' />
+						</Head>
+						<Nav />
+						<main className={styles.main}>{children}</main>
+						<Footer />
+					</div>
+				</ThemeContextProvider>
+			</HostContextProvider>
 		</SessionProvider>
 	);
 };
