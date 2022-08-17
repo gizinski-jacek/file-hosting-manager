e6be9b02b97e34dc373a3672ef8413de2ea082f8
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-17 19:30:03 +0200

    Updating and cleaning up.
    Moving host control buttons to SideMenu component.
    Removing state, importing HostContext to render correct host component.

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,68 +1,25 @@
-import { Button } from '@mui/material';
 import type { NextPage } from 'next';
-import { useState } from 'react';
 import styles from '../../styles/Dashboard.module.scss';
 import Gofile from '../../components/hosts/Gofile';
 import Pixeldrain from '../../components/hosts/Pixeldrain';
 import Mixdrop from '../../components/hosts/Mixdrop';
 import Anonfiles from '../../components/hosts/Anonfiles';
+import { HostContext } from '../../hooks/HostProvider';
+import { useContext } from 'react';
 
 const Dashboard: NextPage = () => {
-	const [selectedFileHost, setSelectedFileHost] = useState('');
-
-	const handleFileHostChange = (e: React.MouseEvent<HTMLButtonElement>) => {
-		const value = (e.target as HTMLButtonElement).value;
-		setSelectedFileHost(value);
-	};
+	const { host } = useContext(HostContext);
 
 	return (
 		<div className={styles.dashboard}>
-			<div className={styles.social_media_controls}>
-				<Button
-					size='small'
-					variant='outlined'
-					color='primary'
-					value='gofile'
-					onClick={(e) => handleFileHostChange(e)}
-				>
-					Gofile
-				</Button>
-				<Button
-					size='small'
-					variant='outlined'
-					color='info'
-					value='pixeldrain'
-					onClick={(e) => handleFileHostChange(e)}
-				>
-					Pixeldrain
-				</Button>
-				<Button
-					size='small'
-					variant='outlined'
-					color='secondary'
-					value='mixdrop'
-					onClick={(e) => handleFileHostChange(e)}
-				>
-					Mixdrop
-				</Button>
-				<Button
-					size='small'
-					variant='outlined'
-					color='warning'
-					value='anonfiles'
-					onClick={(e) => handleFileHostChange(e)}
-				>
-					Anonfiles
-				</Button>
-			</div>
 			<div className={styles.contents}>
-				{selectedFileHost === 'gofile' ? (
+				{host == 'gofile' ? (
 					<Gofile />
-				) : selectedFileHost === 'pixeldrain' ? (
+				) : host == 'pixeldrain' ? (
 					<Pixeldrain />
-				) : selectedFileHost === 'mixdrop' ? (
+				) : host == 'mixdrop' ? (
 					<Mixdrop />
-				) : selectedFileHost === 'anonfiles' ? (
+				) : host == 'anonfiles' ? (
 					<Anonfiles />
 				) : (
 					<div>Choose file host and provide API key to manage your files</div>
