eac94d9ee4fb83a1a78bb2368721474aa3e527b1
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-17 18:35:24 +0200

    Moving nav controls to SideMenu component.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,62 +1,13 @@
-import { Button, Link } from '@mui/material';
-import { useSession } from 'next-auth/react';
-import { signOut } from 'next-auth/react';
+import { Box, Button, IconButton, Link, Typography } from '@mui/material';
+import { useState } from 'react';
 import styles from '../styles/Nav.module.scss';
+import SideMenu from './SideMenu';
 
 const Nav = () => {
-	const { data: user } = useSession();
-
 	return (
-		<div className={styles.nav}>
-			<div className={styles.left}>
-				<Link href='/'>File Hosting Manager</Link>
-			</div>
-			<div className={styles.right}>
-				<Button
-					component='a'
-					href='/dashboard'
-					size='small'
-					variant='outlined'
-					color='warning'
-				>
-					Dashboard
-				</Button>
-				{user ? (
-					<Button
-						size='small'
-						variant='outlined'
-						color='secondary'
-						onClick={() => signOut({ callbackUrl: '/' })}
-					>
-						Sign Out
-					</Button>
-				) : null}
-				{user ? (
-					<Button
-						component='a'
-						href='/account'
-						size='small'
-						variant='outlined'
-						color='secondary'
-					>
-						{user?.user?.username}
-					</Button>
-				) : (
-					<Button
-						component='a'
-						href='/'
-						size='small'
-						variant='outlined'
-						color='secondary'
-					>
-						Sign In
-					</Button>
-				)}
-				<Button size='small' variant='outlined' color='primary'>
-					Toggle D/L Mode
-				</Button>
-			</div>
-		</div>
+		<Box className={styles.nav} sx={{ display: 'flex', height: '64px' }}>
+			<SideMenu />
+		</Box>
 	);
 };
 
