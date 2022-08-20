820c6a4abcc0e92a51a09a18fd82c035fcbb1579
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-20 17:05:36 +0200

    Adding multiple file upload functionality.
    Adding modal with file input form.
    Adding necessary handlers and state.
    Adding function for creating FormData with files and optional new folder name.
    Importing data wrapper and updating file data rendering part of code.

--- components/hosts/Pixeldrain.tsx
+++ components/hosts/Pixeldrain.tsx
@@ -1,16 +1,34 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import {
+	Box,
 	Button,
 	FormControl,
 	FormGroup,
+	FormHelperText,
 	Grid,
 	Input,
 	InputLabel,
+	List,
+	ListItem,
+	Modal,
 } from '@mui/material';
 import { Refresh } from '@mui/icons-material';
 import { APIFormData, PixeldrainFile } from '../../lib/types/types';
 import { useSession } from 'next-auth/react';
+import FileDataWrapper from '../../lib/wrappers/FileDataWrapper';
+
+const style = {
+	position: 'absolute' as 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	bgcolor: 'white',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
+};
 
 const Pixeldrain = () => {
 	const { data: user } = useSession();
@@ -22,8 +40,14 @@ const Pixeldrain = () => {
 	const [filesData, setFilesData] = useState<PixeldrainFile[]>();
 	const [foldersData, setFoldersData] = useState<PixeldrainFile[]>();
 	const [selectedFolder, setSelectedFolder] = useState('root');
+	const [openModal, setOpenModal] = useState(false);
+	const [createFolderInput, setCreateFolderInput] = useState('');
+	const [uploadData, setUploadData] = useState<File[]>([]);
+	const [uploadErrors, setUploadErrors] = useState<string[]>([]);
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const fileRef = useRef<HTMLInputElement>(null);
+
+	const handleKeyInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
 		setFormData((prevState) => ({
 			...prevState,
@@ -31,16 +55,22 @@ const Pixeldrain = () => {
 		}));
 	};
 
-	const handleFormSubmit = async () => {
+	const handleKeyFormSubmit = async () => {
 		try {
 			if (user) {
-				await axios.put('/api/user/account-data', formData, {
+				const res = await axios.put('/api/user/account-data', formData, {
 					withCredentials: true,
 				});
+				if (res.status === 200) {
+					setUserHasAPIKey(true);
+				}
 			} else {
-				await axios.post('/api/user/temp-user', formData, {
+				const res = await axios.post('/api/user/temp-user', formData, {
 					withCredentials: true,
 				});
+				if (res.status === 200) {
+					setUserHasAPIKey(true);
+				}
 			}
 		} catch (error) {
 			console.log(error);
@@ -81,7 +111,7 @@ const Pixeldrain = () => {
 		}
 	}, [selectedFolder, userHasAPIKey]);
 
-	const handleGetSingleFileData = async (fileId: string, fileName: string) => {
+	const handleGetSingleFile = async (fileId: string, fileName: string) => {
 		try {
 			const res: AxiosResponse = await axios.get(
 				`/api/host/pixeldrain/get-single-file?id=${fileId}`,
@@ -99,8 +129,103 @@ const Pixeldrain = () => {
 		}
 	};
 
+	const handleFolderInputChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setCreateFolderInput(e.target.value);
+	};
+
+	const clickSelectFiles = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		fileRef.current?.click();
+	};
+
+	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUploadErrors([]);
+		const target = e.target as HTMLInputElement;
+		const files = target.files as FileList;
+		const errorsArray = [];
+		if (!files) {
+			return;
+		}
+		if (files.length > 500) {
+			errorsArray.push('Max number of files to opload is 500');
+		}
+		for (let i = 0; i < files.length; i++) {
+			if (files[i].name.length > 255) {
+				errorsArray.push('Max file name is 255 characters');
+				break;
+			}
+		}
+		if (errorsArray.length > 0) {
+			setUploadErrors(errorsArray);
+			return;
+		}
+		setUploadData((prevState) => [...prevState, ...files]);
+	};
+
+	const handleClearFileList = () => {
+		setUploadData([]);
+	};
+
+	const handleRemoveSingleFile = (value: string) => {
+		const newState = uploadData.filter((file) => file.name !== value);
+		setUploadData(newState);
+	};
+
+	const handleUploadFiles = async () => {
+		try {
+			if (!uploadData) {
+				return;
+			}
+			const uploadFormData = new FormData();
+			if (createFolderInput) {
+				uploadFormData.append('folder', createFolderInput);
+			}
+			uploadData.forEach((file, index) => {
+				uploadFormData.append('file_' + index, file);
+			});
+			const res: AxiosResponse = await axios.post(
+				`/api/host/pixeldrain/add-file`,
+				uploadFormData,
+				{ withCredentials: true }
+			);
+			if (res.status === 200) {
+				handleGetFolderFilesData();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleDeleteSingleFile = async (fileId: string) => {
+		try {
+			const res: AxiosResponse = await axios.delete(
+				`/api/host/pixeldrain/delete-file?id=${fileId}`,
+				{
+					withCredentials: true,
+				}
+			);
+			if (res.status === 200) {
+				handleGetFolderFilesData();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	const handleFolderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		setSelectedFolder(e.target.value);
+	};
+
+	const handleOpenModal = () => {
+		setOpenModal(true);
+	};
+
+	const handleCloseModal = () => {
+		setUploadData([]);
+		setCreateFolderInput('');
+		setOpenModal(false);
 	};
 
 	useEffect(() => {
@@ -154,6 +279,76 @@ const Pixeldrain = () => {
 								})}
 							</select>
 						</FormControl>
+						<Box sx={{ mx: 1 }}>
+							<Button type='button' onClick={handleOpenModal}>
+								Upload Files
+							</Button>
+							<Modal open={openModal} onClose={handleCloseModal}>
+								<Box sx={style}>
+									<Box
+										sx={{ display: 'flex', justifyContent: 'space-between' }}
+									>
+										<Button type='button' onClick={clickSelectFiles}>
+											Add Files
+										</Button>
+										<Button type='button' onClick={handleClearFileList}>
+											Clear List
+										</Button>
+									</Box>
+									<List>
+										{uploadData.map((file) => {
+											return (
+												<ListItem disablePadding key={file.name}>
+													{file.name}
+													<Button
+														type='button'
+														sx={{ ml: 'auto' }}
+														onClick={() => handleRemoveSingleFile(file.name)}
+													>
+														X
+													</Button>
+												</ListItem>
+											);
+										})}
+									</List>
+									<FormGroup>
+										<FormControl>
+											<InputLabel htmlFor='create_folder'>
+												Add files to new folder?
+											</InputLabel>
+											<Input
+												id='create_folder'
+												name='create_folder'
+												type='text'
+												value={createFolderInput}
+												onChange={(e) => handleFolderInputChange(e)}
+												placeholder='Folder name'
+											/>
+										</FormControl>
+										<FormControl sx={{ display: 'none' }}>
+											<label htmlFor='upload_files'>Add files</label>
+											<input
+												ref={fileRef}
+												id='upload_files'
+												name='upload_files'
+												type='file'
+												multiple
+												onChange={handleFileChange}
+												required
+											/>
+										</FormControl>
+										{uploadErrors.map((message, index) => (
+											<FormHelperText sx={{ mx: 1, color: 'red' }} key={index}>
+												{message}
+											</FormHelperText>
+										))}
+										<Button type='button' onClick={handleUploadFiles}>
+											Upload Files
+										</Button>
+									</FormGroup>
+								</Box>
+							</Modal>
+						</Box>
 					</Grid>
 					<div>
 						<Grid container p={2}>
@@ -176,52 +371,12 @@ const Pixeldrain = () => {
 						<Grid container>
 							{filesData?.map((file) => {
 								return (
-									<Grid
-										container
-										alignItems={'center'}
-										mx={2}
+									<FileDataWrapper
 										key={file.id}
-										id={file.id}
-									>
-										<Grid
-											item
-											xs={7}
-											sx={{ display: 'flex' }}
-											justifyContent='space-between'
-											alignItems='center'
-										>
-											{file.name}
-											<Grid item>
-												<Button
-													type='button'
-													onClick={() =>
-														handleGetSingleFileData(file.id, file.name)
-													}
-												>
-													Download
-												</Button>
-												<Button
-													component='a'
-													href={`https://pixeldrain.com/u/${file.id}`}
-													target='_blank'
-												>
-													Direct Link
-												</Button>
-											</Grid>
-										</Grid>
-										<Grid item xs={2} textAlign='end'>
-											{file.date_upload}
-										</Grid>
-										<Grid item xs={1} textAlign='end'>
-											{file.size}
-										</Grid>
-										<Grid item xs={1} textAlign='end'>
-											{file.downloads}
-										</Grid>
-										<Grid item xs={1} textAlign='end'>
-											{file.views}
-										</Grid>
-									</Grid>
+										data={file}
+										getFile={handleGetSingleFile}
+										deleteFile={handleDeleteSingleFile}
+									/>
 								);
 							})}
 						</Grid>
@@ -237,12 +392,12 @@ const Pixeldrain = () => {
 							name='api_key'
 							inputProps={{ minLength: 4, maxLength: 64 }}
 							value={formData.api_key}
-							onChange={handleInputChange}
+							onChange={handleKeyInputChange}
 							required
 							placeholder='API key'
 						/>
 					</FormControl>
-					<Button type='button' onClick={handleFormSubmit}>
+					<Button type='button' onClick={handleKeyFormSubmit}>
 						Submit
 					</Button>
 				</FormGroup>
