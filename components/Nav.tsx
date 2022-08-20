eeacabdca95019a73f0f742e3bfbb969dda5808f
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-20 16:45:15 +0200

    Cleaning up

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,11 @@
-import { Box, Button, IconButton, Link, Typography } from '@mui/material';
-import { useState } from 'react';
 import styles from '../styles/Nav.module.scss';
-import SideMenu from './SideMenu';
+import MiniDrawer from './MiniDrawer';
 
 const Nav = () => {
 	return (
-		<Box className={styles.nav} sx={{ display: 'flex', height: '64px' }}>
-			<SideMenu />
-		</Box>
+		<nav className={styles.nav}>
+			<MiniDrawer />
+		</nav>
 	);
 };
 
