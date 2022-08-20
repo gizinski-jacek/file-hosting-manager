fbace675df30c8eee5e74f7661c7c1e39cc51fd8
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-21 00:15:47 +0200

    Reducing button sizes.
    Adding checkboxes, props state, and handlers for checkboxes.

--- lib/wrappers/FileDataWrapper.tsx
+++ lib/wrappers/FileDataWrapper.tsx
@@ -1,15 +1,34 @@
-import { Button, Grid } from '@mui/material';
+import { Button, Checkbox, FormControl, FormGroup, Grid } from '@mui/material';
 import { PixeldrainFile } from '../types/types';
 
 interface Props {
 	data: PixeldrainFile;
 	getFile: (fileId: string, fileName: string) => void;
-	deleteFile: (fileId: string) => void;
+	checkboxState: boolean;
+	handleCheckbox: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const FileDataWrapper = ({ data, getFile, deleteFile }: Props) => {
+const FileDataWrapper = ({
+	data,
+	getFile,
+	checkboxState,
+	handleCheckbox,
+}: Props) => {
 	return (
 		<Grid container alignItems={'center'} mx={2} id={data.id}>
+			<Grid item>
+				<FormControl>
+					<FormGroup>
+						<Checkbox
+							size='small'
+							color='error'
+							value={data.id}
+							checked={checkboxState}
+							onChange={(e) => handleCheckbox(e)}
+						/>
+					</FormGroup>
+				</FormControl>
+			</Grid>
 			<Grid
 				item
 				xs={7}
@@ -17,20 +36,24 @@ const FileDataWrapper = ({ data, getFile, deleteFile }: Props) => {
 				justifyContent='space-between'
 				alignItems='center'
 			>
-				{data.name}
+				<Grid item xs={7} textAlign='start'>
+					{data.name}
+				</Grid>
 				<Grid item>
-					<Button type='button' onClick={() => getFile(data.id, data.name)}>
+					<Button
+						type='button'
+						size='small'
+						onClick={() => getFile(data.id, data.name)}
+					>
 						Download
 					</Button>
 					<Button
 						component='a'
+						size='small'
 						href={`https://pixeldrain.com/u/${data.id}`}
 						target='_blank'
 					>
 						Direct Link
-					</Button>
-					<Button type='button' onClick={() => deleteFile(data.id)}>
-						Delete
 					</Button>
 				</Grid>
 			</Grid>
