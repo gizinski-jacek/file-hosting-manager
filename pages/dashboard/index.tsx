2724c9967b57e9915252242b1fb3f26e7d5f39ec
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-22 01:58:13 +0200

    Adding grid and grid buttons with supported host files.

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -6,26 +6,56 @@ import Mixdrop from '../../components/hosts/Mixdrop';
 import Anonfiles from '../../components/hosts/Anonfiles';
 import { HostContext } from '../../hooks/HostProvider';
 import { useContext } from 'react';
+import { Box, Button, Grid } from '@mui/material';
+import { supportedHostList } from '../../lib/defaults';
 
 const Dashboard: NextPage = () => {
-	const { host } = useContext(HostContext);
+	const { host, updateHost } = useContext(HostContext);
 
 	return (
-		<div className={styles.dashboard}>
-			<div className={styles.contents}>
-				{host == 'gofile' ? (
-					<Gofile />
-				) : host == 'pixeldrain' ? (
-					<Pixeldrain />
-				) : host == 'mixdrop' ? (
-					<Mixdrop />
-				) : host == 'anonfiles' ? (
-					<Anonfiles />
-				) : (
-					<div>Choose file host and provide API key to manage your files</div>
-				)}
-			</div>
-		</div>
+		<Box
+			sx={{
+				flex: 1,
+				mt: '64px',
+				ml: '64px',
+				height: 'calc(100vh - 64px)',
+				maxHeight: 'calc(100vh - 64px)',
+				width: 'calc(100vw - 64px)',
+				maxWidth: 'calc(100vw - 64px)',
+				overflow: 'hidden',
+			}}
+		>
+			{host ? (
+				<Box sx={{ width: '100%', height: '100%', overflow: 'auto' }}>
+					{host == 'gofile' ? (
+						<Gofile />
+					) : host == 'pixeldrain' ? (
+						<Pixeldrain />
+					) : host == 'mixdrop' ? (
+						<Mixdrop />
+					) : host == 'anonfiles' ? (
+						<Anonfiles />
+					) : null}
+				</Box>
+			) : (
+				<Box sx={{ mx: 'auto', maxWidth: '75%' }}>
+					<Box sx={{ m: 2, textAlign: 'center' }}>
+						<h2>
+							Select file host and provide API credentials to manage your files
+						</h2>
+					</Box>
+					<Grid container p={2}>
+						{supportedHostList.map((host, index) => {
+							return (
+								<Grid key={index} item xs={3} sx={{ textAlign: 'center' }}>
+									<Button onClick={() => updateHost(host)}>{host}</Button>
+								</Grid>
+							);
+						})}
+					</Grid>
+				</Box>
+			)}
+		</Box>
 	);
 };
 
