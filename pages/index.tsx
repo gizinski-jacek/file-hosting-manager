8751b3cb246263470ffb619a1c376e1f38c55cf0
Jacek Gizinski <jacektrg+github@gmail.com>
2022-08-23 22:39:01 +0200

    Minor cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import { signIn } from 'next-auth/react';
